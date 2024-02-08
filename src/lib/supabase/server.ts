85950178bc06596efb73eb5d44d3b458b1465f4b
ninthsun91 <ninthsun91@gmail.com>
2024-02-08 16:05:38 +0900

    Create login page

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,7 +1,8 @@
 import { createServerClient, CookieOptions } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-export function createClient(cookieStore: ReturnType<typeof cookies>) {
+export function createClient() {
+  const cookieStore = cookies();
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
